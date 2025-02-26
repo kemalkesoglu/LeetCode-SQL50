be4f22f179bccfb236eec3dd43db8317ff228151
kemalkesoglu <kemalkesoglu@gmail.com>
2025-02-26 19:34:33 +0300

    Select.sql

--- Select.sql
+++ Select.sql
@@ -1,16 +1,20 @@
---Recyclable and Low Fat Products--
+--Recyclable and Low Fat Products
 SELECT product_id FROM products
 WHERE low_fats = 'Y'
 AND recyclable = 'Y'
 
---Find Customer Referee--
+--Find Customer Referee
 SELECT name FROM Customer
  WHERE referee_id<>2 OR referee_id is null
 
---Big Countries--
+--Big Countries
 SELECT name,population,area FROM World
 WHERE population >= 25000000 OR area >= 3000000 
 
---Article Views I--
+--Article Views I
 SELECT DISTINCT author_id AS id FROM Views
 WHERE author_id=viewer_id
+
+--Invalid Tweets
+SELECT tweet_id FROM Tweets
+WHERE LEN(content)>15
