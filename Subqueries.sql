f29725657ae4feae7ee06bb1910e131157bb31f0
kemalkesoglu <kemalkesoglu@gmail.com>
2025-03-22 17:29:37 +0300

    Subqueries.sql

--- Subqueries.sql
+++ Subqueries.sql
@@ -14,7 +14,7 @@ CASE
  END AS id,student
 FROM Seat
 ORDER BY id
-
+ 
 --Movie Rating
 SELECT*FROM
  (SELECT TOP 1 U.name AS results
@@ -32,6 +32,15 @@ WHERE MR.created_at BETWEEN '2020-02-01' AND '2020-02-29'
 ORDER BY AVG(MR.rating*1.0) OVER(PARTITION BY M.title) DESC, M.title ASC
 ) N2
 
+--Restaurant Growth
+SELECT   visited_on, 
+SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)   AS'amount',
+ROUND(CAST(SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS FLOAT)/7.0 ,2) AS'average_amount' 
+FROM     Customer 
+GROUP BY visited_on
+ORDER BY visited_on
+OFFSET 6 ROWS  
+ 
 --Friend Requests II: Who Has the Most Friends
 SELECT TOP 1 id, COUNT(*) AS num
     FROM (SELECT requester_id AS id
