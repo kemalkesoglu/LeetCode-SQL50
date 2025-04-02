68fc5f334f9e8eb0993e71e659dd2304ef06aeb7
Kemal KEŞOĞLU <kemalkesoglu@gmail.com>
2025-04-02 22:21:30 +0300

    Basic Aggregate Functions.sql

--- Basic Aggregate Functions.sql
+++ Basic Aggregate Functions.sql
@@ -40,4 +40,4 @@ SELECT
      SUM(amount) AS trans_total_amount,
      SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY FORMAT(trans_date,'yyyy-MM'),country
+GROUP BY FORMAT(trans_date,'yyyy-MM'),country 
